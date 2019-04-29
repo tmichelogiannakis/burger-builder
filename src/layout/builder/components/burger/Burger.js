256baa87d0640b4d7db2661cba2bdca3cde9329c
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2019-04-30 01:28:41 +0300

    refactor: burger css

--- src/layout/builder/components/burger/Burger.js
+++ src/layout/builder/components/burger/Burger.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Ingredient from './ingredient/Ingredient';
 
-const burger = () => {
+const burger = ({ className }) => {
   return (
-    <div style={{ height: '640px' }}>
+    <div className={className}>
       <Ingredient type="bread-top" />
       <Ingredient type="meat" />
       <Ingredient type="salad" />
-      <Ingredient type="chesse" />
+      <Ingredient type="cheese" />
       <Ingredient type="bacon" />
       <Ingredient type="bread-bottom" />
     </div>
   );
 };
 
-export default burger;
+export default styled(burger)`
+  width: 100%;
+  margin: auto;
+  height: 250px;
+  overflow: auto;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+  @media (min-width: 500px) and (min-height: 400px) {
+    width: 350px;
+    height: 300px;
+  }
+
+  @media (min-width: 500px) and (min-height: 401px) {
+    width: 450px;
+    height: 400px;
+  }
+
+  @media (min-width: 1000px) and (min-height: 700px) {
+    width: 700px;
+    height: 600px;
+  }
+`;
